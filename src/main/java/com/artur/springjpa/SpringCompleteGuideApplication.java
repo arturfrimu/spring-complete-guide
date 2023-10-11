e1927078ceb717840ae04fbca5a0a8e81aa1e3c6
Artur Frimu <artur.frimu2@gmail.com>
2023-10-11 12:02:37 +0300

    rename project and add local database

--- src/main/java/com/artur/springjpa/SpringCompleteGuideApplication.java
+++ src/main/java/com/artur/springjpa/SpringCompleteGuideApplication.java
@@ -2,10 +2,12 @@ package com.artur.springjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-public class SpringJpaApplication {
+@EnableJpaRepositories
+public class SpringCompleteGuideApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringJpaApplication.class, args);
+        SpringApplication.run(SpringCompleteGuideApplication.class, args);
     }
 }
