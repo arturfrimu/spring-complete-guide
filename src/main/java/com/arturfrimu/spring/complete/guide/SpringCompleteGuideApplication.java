48bd2233e3c180dc1098a82907dafa5cef4d45ba
Artur Frimu <artur.frimu2@gmail.com>
2024-03-21 23:31:57 +0200

    @EnableCaching

--- src/main/java/com/arturfrimu/spring/complete/guide/SpringCompleteGuideApplication.java
+++ src/main/java/com/arturfrimu/spring/complete/guide/SpringCompleteGuideApplication.java
@@ -2,10 +2,12 @@ package com.arturfrimu.spring.complete.guide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableCaching
 public class SpringCompleteGuideApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringCompleteGuideApplication.class, args);
