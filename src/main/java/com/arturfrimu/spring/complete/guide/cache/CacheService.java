953f8c47a61912a6ae02070362c89194d269b3a9
Artur Frimu <artur.frimu2@gmail.com>
2024-03-22 00:27:31 +0200

    Add CacheManager cacheManager

--- src/main/java/com/arturfrimu/spring/complete/guide/cache/CacheService.java
+++ src/main/java/com/arturfrimu/spring/complete/guide/cache/CacheService.java
@@ -1,6 +1,8 @@
 package com.arturfrimu.spring.complete.guide.cache;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Service;
 public class CacheService {
 
     private final CacheDao cacheDao;
+    private final CacheManager cacheManager;
 
     @Cacheable("data")
     public String getData(String key) {
@@ -23,5 +26,19 @@ public class CacheService {
     }
 
     @CacheEvict(value = "data", allEntries = true)
-    public void cacheEvict() {}
+    public void cacheEvict() {
+    }
+
+    @Cacheable("data")
+    public String getDataManual(String key) {
+        Cache data = cacheManager.getCache("data");
+//
+//        if (Objects.nonNull(data)) {
+//            data.get(key);
+//            data.put(key, cacheDao.getData(key));
+//            data.evict(key);
+//        }
+
+        return cacheDao.getData(key);
+    }
 }
