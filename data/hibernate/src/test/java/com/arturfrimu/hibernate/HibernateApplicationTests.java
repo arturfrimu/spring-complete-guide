a943e68529f658aa2036254181c87e43d63ceae7
Artur Frimu <artur.frimu2@gmail.com>
2024-06-10 16:03:45 +0300

    Add hibernate module

---
+++ data/hibernate/src/test/java/com/arturfrimu/hibernate/HibernateApplicationTests.java
@@ -0,0 +1,20 @@
+package com.arturfrimu.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class HibernateApplicationTests {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    void contextLoads() {
+        assertThat(applicationContext).isNotNull();
+    }
+}
