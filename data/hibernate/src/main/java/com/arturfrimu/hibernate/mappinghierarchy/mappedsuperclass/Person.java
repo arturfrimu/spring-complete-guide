74b56677f38f9b21d67388ffa5e0c0607ae71906
Artur Frimu <artur.frimu2@gmail.com>
2024-06-11 08:16:43 +0300

    Add hibernate mapped superclass hierarchy

---
+++ data/hibernate/src/main/java/com/arturfrimu/hibernate/mappinghierarchy/mappedsuperclass/Person.java
@@ -0,0 +1,29 @@
+package com.arturfrimu.hibernate.mappinghierarchy.mappedsuperclass;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@SuperBuilder
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long personId;
+    private String name;
+}
+
+
+
