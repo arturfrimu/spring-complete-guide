dc98f540b87c5aca41007e9d46341cd16c7bc6f5
Frimu Artur <artur@tecorien.com>
2024-09-01 12:39:02 +0300

    Add validation on Authorization : Bearer

--- security/jwt-bouali-ali/src/main/java/com/arturfrimu/jwt/config/JwtAuthenticationFilter.java
+++ security/jwt-bouali-ali/src/main/java/com/arturfrimu/jwt/config/JwtAuthenticationFilter.java
@@ -4,7 +4,9 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,7 +17,19 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(
+            @NotNull HttpServletRequest request,
+            @NotNull HttpServletResponse response,
+            @NotNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (authorization == null || !authorization.startsWith("Bearer ")) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("No JWT token found or token does not start with 'Bearer '");
+            return;
+        }
+
         filterChain.doFilter(request, response);
     }
 }
