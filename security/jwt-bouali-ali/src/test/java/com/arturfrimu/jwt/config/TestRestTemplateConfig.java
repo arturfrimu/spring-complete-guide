accc437513e6e9aab9d1451c6d1cb0cccd4d0860
Frimu Artur <artur@tecorien.com>
2024-09-01 11:42:00 +0300

    Add TestRestTemplateConfig

---
+++ security/jwt-bouali-ali/src/test/java/com/arturfrimu/jwt/config/TestRestTemplateConfig.java
@@ -0,0 +1,13 @@
+package com.arturfrimu.jwt.config;
+
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestRestTemplateConfig {
+    @Bean
+    public TestRestTemplate testRestTemplate() {
+        return new TestRestTemplate();
+    }
+}
