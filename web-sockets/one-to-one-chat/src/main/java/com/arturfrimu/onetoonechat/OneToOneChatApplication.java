e6e99b549f720e00a08e9cb951581495f2ee9097
Frimu Artur <artur@tecorien.com>
2024-09-02 23:48:56 +0300

    Add initial commit

---
+++ web-sockets/one-to-one-chat/src/main/java/com/arturfrimu/onetoonechat/OneToOneChatApplication.java
@@ -0,0 +1,11 @@
+package com.arturfrimu.onetoonechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToOneChatApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OneToOneChatApplication.class, args);
+    }
+}
