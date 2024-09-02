7d5a09997a77541da90e1878e3a31b994885f167
Frimu Artur <artur@tecorien.com>
2024-09-03 00:25:13 +0300

    Change name in open api config

--- security/jwt-bouali-ali/src/main/java/com/arturfrimu/jwt/config/OpenApiConfig.java
+++ security/jwt-bouali-ali/src/main/java/com/arturfrimu/jwt/config/OpenApiConfig.java
@@ -13,12 +13,12 @@ import io.swagger.v3.oas.annotations.servers.Server;
 @OpenAPIDefinition(
         info = @Info(
                 contact = @Contact(
-                        name = "Alibou",
-                        email = "contact@aliboucoding.com",
-                        url = "https://aliboucoding.com/course"
+                        name = "Test",
+                        email = "contact@testcoding.com",
+                        url = "https://testcoding.com/course"
                 ),
                 description = "OpenApi documentation for Spring Security",
-                title = "OpenApi specification - Alibou",
+                title = "OpenApi specification - Test",
                 version = "1.0",
                 license = @License(
                         name = "Licence name",
@@ -33,7 +33,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
                 ),
                 @Server(
                         description = "PROD ENV",
-                        url = "https://aliboucoding.com/course"
+                        url = "https://testcoding.com/course"
                 )
         },
         security = {
