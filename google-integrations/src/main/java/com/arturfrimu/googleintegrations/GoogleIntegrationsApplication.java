10036d8c611ae56e4fe9d412d0e1389fe0485a84
arturfrimu <artur.frimu2@gmail.com>
2024-11-20 23:41:51 +0200

    Add google integration module

---
+++ google-integrations/src/main/java/com/arturfrimu/googleintegrations/GoogleIntegrationsApplication.java
@@ -0,0 +1,13 @@
+package com.arturfrimu.googleintegrations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleIntegrationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleIntegrationsApplication.class, args);
+    }
+
+}
---
+++ google-integrations/src/test/java/com/arturfrimu/googleintegrations/GoogleIntegrationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturfrimu.googleintegrations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleIntegrationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
