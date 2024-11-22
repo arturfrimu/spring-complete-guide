dfe0bd8ce878b66894400d7caf0d790abf9abc8a
arturfrimu <artur.frimu2@gmail.com>
2024-11-22 09:16:55 +0200

    Change assertThat(applicationContext).isNotNull();

--- google-integrations/src/test/java/com/arturfrimu/googleintegrations/GoogleIntegrationsApplicationTests.java
+++ google-integrations/src/test/java/com/arturfrimu/googleintegrations/GoogleIntegrationsApplicationTests.java
@@ -2,12 +2,17 @@ package com.arturfrimu.googleintegrations;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class GoogleIntegrationsApplicationTests {
 
+    ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        assertThat(applicationContext).isNotNull();
     }
-
 }
--- graphql/src/test/java/com/arturfrimu/graphql/GraphqlApplicationTests.java
+++ graphql/src/test/java/com/arturfrimu/graphql/GraphqlApplicationTests.java
@@ -2,12 +2,16 @@ package com.arturfrimu.graphql;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class GraphqlApplicationTests {
+    ApplicationContext applicationContext;
 
     @Test
     void contextLoads() {
+        assertThat(applicationContext).isNotNull();
     }
-
 }
