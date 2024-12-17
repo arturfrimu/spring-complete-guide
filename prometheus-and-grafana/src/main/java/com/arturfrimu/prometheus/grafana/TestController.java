89a60897be600a48215ee377c96dea93a6027989
arturfrimu <artur.frimu2@gmail.com>
2024-12-17 20:10:33 +0200

    Add TestController

---
+++ prometheus-and-grafana/src/main/java/com/arturfrimu/prometheus/grafana/TestController.java
@@ -0,0 +1,13 @@
+package com.arturfrimu.prometheus.grafana;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/monitor")
+    public String monitor() {
+        return "Hello world";
+    }
+}
